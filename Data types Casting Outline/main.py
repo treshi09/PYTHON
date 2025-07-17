16c04a1a983eb3a55e7dec9c24caa9192fd2260f
Your Name <you@example.com>
2025-07-17 17:40:18 +0100

    commit

---
+++ Data types Casting Outline/main.py
@@ -0,0 +1,10 @@
+#Store your Name, Age, Weight, Whether you are a student or not (True for yes, False for no) in respective variables. What do you think is the data type of each variable? Print the data type of every variable. Change the datatype of Age to string and Weight to an integer.
+Name="Treasure"
+Age=9
+Weight=45.5
+student=True
+
+print(type(Name))
+print(type(Age))
+print(type(Weight))
+print(type(student))
